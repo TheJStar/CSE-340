0bc3ee46dbd15835e36c130f4d463a4727dc9214
TheeJStar <kingoflightj@gmail.com>
2024-09-19 03:35:46 -0700

    setup

--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * Veiw Engien and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"}) // this is where you can add all of the variables as pair values like ...{title: "Home", newVar: "value"}) and then use it in the .ejs files by <%- newVar %> || don't use body as a variable because it will replace the default value of the index.ejs
+})
 
 /* ***********************
  * Local Server Information
